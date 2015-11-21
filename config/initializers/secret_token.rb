c883a488b844a03159cc0cf7d55e246de4091cf4
eric orellana <eric.orellana92@gmail.com>
2015-11-21 18:43:59 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flickme::Application.config.secret_key_base = '6b6f6e7ed083c44e22b4c5911bbcefceba2d0435d6b6025517e6bf334cc3dea168afb844abd5128d504458c57705a48860d95ca29106d90204902076e1bbe55e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Flickme::Application.config.session_store :cookie_store, key: '_flickme_session'
