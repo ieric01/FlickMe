6fdb2fca54cd53ca09d0267962a138a6fb8e3bd5
eric orellana <eric.orellana92@gmail.com>
2015-11-23 16:23:54 +0000

    added lessons to each section, ability to create lessons, and lessons display on instructor course page

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,17 @@ Flickme::Application.routes.draw do
   root 'static_pages#index'
   resources :courses, :only => [:index, :show]
   namespace :instructor do 
-  	resources :courses, :only => [:new, :create, :show] do
-  		resources :sections, :only => [:new, :create]
+  	resources :sections, :only => [] do
+  		resources :lessons, :only => [:new, :create]
   	end
+  	resources :courses, :only => [:new, :create, :show] do
+      resources :sections, :only => [:new, :create]
+    end	
   end
 end
+
+	# resources :courses, :only => [:new, :create, :show] do
+  	#   resources :sections, :only => [:new, :create] do
+  	# 	resources :lessons, :only => [:new, :create]
+  	#   end
+  	# end
