eef64b01f99a3eb6743e7482080e6feda6ed0259
eric orellana <eric.orellana92@gmail.com>
2015-11-24 16:21:41 +0000

    resized images with mini magick

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'pry-nav'
 gem 'carrierwave'
 gem "figaro"
 gem "fog", "~> 1.3.1"
+gem 'mini_magick', '~> 4.3.1'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,6 @@
-
-# config/initializers/carrierwave.rb
-
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',                        # required
+    :provider               => 'AWS',                     
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
     :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
   }
